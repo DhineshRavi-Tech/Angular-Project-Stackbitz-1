b5c621baa51846087db44b211dfe1d8e89c99757
DhineshRavi-Tech <dineshravi797@gmail.com>
2022-06-29 12:20:14 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, MatInputModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
